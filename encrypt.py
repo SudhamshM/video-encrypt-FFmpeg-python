ab629c4fd1f7532202638378488977bb13b6c4de
Sudhamsh Mondrati <sudhamshmondrati@gmail.com>
2023-03-06 11:14:14 -0500

    added check to make sure path exists

--- encrypt.py
+++ encrypt.py
@@ -1,16 +1,21 @@
-import ffmpeg, secrets, sys
+import ffmpeg, secrets, sys, os
 
 
 def encrypt(video_name=""):
     """Encrypt an mp4 using CENC-AES-CTR scheme.
 
     Args:
-        video_name (str): The video to encrypt.
+        video_name (str): The video name to encrypt without the extension.
     """
     if len(sys.argv) > 1:
         video_name = sys.argv[1]
+    
     if video_name == "":
         print("No file name specified.")
+        return
+    
+    if not os.path.isfile(video_name + ".mp4"):
+        print("File doesn't exist, please provide correct path.")
         return
     
     secret_key = secrets.token_hex(16)
